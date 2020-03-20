ee68a3cd0198834dec4ddb656077205932121365
Wessel_van_Kasteel <wesselvankasteel@ziggo.nl>
2020-03-20 10:05:54 -0400

    controller added

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -1,10 +1,15 @@
 <?php
 
+
+
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\account;
 
 class AccountController extends Controller
 {
-    //
+  public function show(){
+    return Account::all();
+  }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/account','AccountController@show');
+
 Route::get('/', function () {
     return view('welcome');
 });
