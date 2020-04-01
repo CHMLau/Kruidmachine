8ccd62939ae63bfeef9e64280b8851b981e88a2b
Wessel_van_Kasteel <wesselvankasteel@ziggo.nl>
2020-04-01 09:55:42 +0200

    transfer from pi to laptop

--- main.py
+++ main.py
@@ -15,12 +15,14 @@ mycursor = mydb.cursor()
 
 port = serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout=3.0)
 
+os.system("python mix_uit.py")
+
 while True:
 
     port.write("c0")
     mydb.commit()
 
-    mycursor.execute("SELECT hoeveelheid_comp_1 FROM mixes WHERE maken = 'ja';")
+    mycursor.execute("SELECT hoeveelheid1 FROM mix WHERE maken = 'ja';")
 
     for x in mycursor:
         print("mix maken: " + x[0])
@@ -29,15 +31,15 @@ while True:
             port.write("c1")
             mydb.commit()
             print("comp1: theelepel")
-            os.system("python mix_uit.py")
 
         elif (x[0] == '1/2 theelepel'):
             port.write("c2")
-            print("comp1: 1/2 theelepel")
-            os.system("python mix_uit.py")
+            print("comp1: 1/2 theelepel")   
+      
             #hier de tijd die nodig is per compartiment ook doorsturen als we met miligrammen gaan werken
 
-    mycursor.execute("SELECT hoeveelheid_comp_2 FROM mixes WHERE maken = 'ja';")
+	time.sleep(8)
+    mycursor.execute("SELECT hoeveelheid2 FROM mix WHERE maken = 'ja';")
 
     for x in mycursor:
         # print("mix maken: " + x[0])
@@ -46,15 +48,12 @@ while True:
             port.write("c3")
             mydb.commit()
             print("comp2: theelepel")
-            os.system("python mix_uit.py")
 
         elif (x[0] == '1/2 theelepel'):
             port.write("c4")
             print("comp2: 1/2 theelepel")
-            os.system("python mix_uit.py")
 
-
-    mycursor.execute("SELECT hoeveelheid_comp_3 FROM mixes WHERE maken = 'ja';")
+    mycursor.execute("SELECT hoeveelheid3 FROM mix WHERE maken = 'ja';")
 
     for x in mycursor:
 
@@ -62,6 +61,7 @@ while True:
             port.write("c5")
             mydb.commit()
             print("comp3: theelepel")
+            time.sleep(1)
             os.system("python mix_uit.py")
 
         elif (x[0] == '1/2 theelepel'):
@@ -72,70 +72,58 @@ while True:
         else:
             os.system("python mix_uit.py")
             print("mix maken uit")
-
+	
     rcv = port.readline().strip()
 
     if (rcv == 'C1_OUT'):
-        os.system('cd comp1/')
         os.system('python comp1_100.py')
         print("Comp1 = 100%")
 
     elif (rcv == 'C1_50'):
-        os.system('cd comp1/')
         os.system("python comp1_50.py")
         print("Comp1 = 50%")
 
     elif (rcv == 'C1_25'):
-        os.system('cd comp1/')
         os.system("python comp1_25.py")
         print("Comp1 = 25%")
 
     elif (rcv == 'C1_0'):
-        os.system('cd comp1/')
         os.system("python comp1_0.py")
         print("Comp1 = 0%")
 
 # ------------------------------------------
 
     if (rcv == 'C2_OUT'):
-        os.system('cd comp2/')
         os.system('python comp2_100.py')
         print("Comp2 = 100%")
 
     elif (rcv == 'C2_50'):
-        os.system('cd comp2/')
         os.system("python comp2_50.py")
         print("Comp2 = 50%")
 
     elif (rcv == 'C2_25'):
-        os.system('cd comp2/')
         os.system("python comp2_25.py")
         print("Comp2 = 25%")
 
     elif (rcv == 'C2_0'):
-        os.system('cd comp2/')
         os.system("python comp2_0.py")
         print("Comp2 = 0%")
 
 # ------------------------------------------
 
     if (rcv == 'C3_OUT'):
-        os.system('cd comp3/')
         os.system('python comp3_100.py')
         print("Comp3 = 100%")
 
     elif (rcv == 'C3_50'):
-        os.system('cd comp3/')
         os.system("python comp3_50.py")
         print("Comp3 = 50%")
 
     elif (rcv == 'C3_25'):
-        os.system('cd comp3/')
         os.system("python comp3_25.py")
         print("Comp3 = 25%")
 
     elif (rcv == 'C3_0'):
-        os.system('cd comp3/')
         os.system("python comp3_0.py")
         print("Comp3 = 0%")
 
